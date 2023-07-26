27b792726a18d6a3d524989151a048d88510dc0b
rohandass58 <rohandass58@gmail.com>
2023-07-26 22:12:15 +0530

    final commit

--- todo_app/models.py
+++ todo_app/models.py
@@ -24,7 +24,7 @@ class Task(models.Model):
         max_length=100,
         null=False,
     )
-    task_description = models.CharField(
+    task_description = models.TextField(
         max_length=300,
         null=True,
         blank=True,
